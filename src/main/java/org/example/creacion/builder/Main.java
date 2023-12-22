9c1c3d2d12cd95b915758b9947422be4c326efc9
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2023-12-21 22:19:40 -0300

    Builder

---
+++ src/main/java/org/example/creacion/builder/Main.java
@@ -0,0 +1,25 @@
+package org.example.creacion.builder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        probarBuilder();
+    }
+
+    private static void probarBuilder(){
+        Card card = new Card.CardBuilder("VISA",
+                "0000 1111 2222 3333")
+                .name("Alberto")
+                .expires(2030)
+                .credit(true)
+                .build();
+
+        System.out.println(card);
+
+        Card card2 = new Card.CardBuilder("AMEX",
+                "9999 9999 9999 9999")
+                .build();
+
+        System.out.println(card2);
+    }
+}
